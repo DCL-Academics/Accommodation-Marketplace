7539ee367c2732cce87f50dc6ae75bd379b1c8db
VARUN A <Varunarya463@gmail.com>
2025-10-09 16:54:41 +0530

    removed unused imports

--- src/main/java/com/dcl/accommodate/security/JwtService.java
+++ src/main/java/com/dcl/accommodate/security/JwtService.java
@@ -5,10 +5,8 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.time.Duration;
 import java.util.Base64;
